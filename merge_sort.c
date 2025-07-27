63a2ded68000c8ebad6980f68579f49a176e3b27
KarmaAndEmptiness <m202240473@xs.ustb.edu.cn>
2025-07-27 05:39:40 +0000

    归并排序

---
+++ merge_sort.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+void merge(int nums[], int left, int mid, int right)
+{
+  int i = left, j = mid + 1, k = 0;
+  int tmp[right - left + 1], len = sizeof tmp / sizeof tmp[0];
+  while (i <= mid && j <= right)
+  {
+    if (nums[i] <= nums[j])
+    {
+      tmp[k++] = nums[i++];
+    }
+    else
+    {
+      tmp[k++] = nums[j++];
+    }
+  }
+  while (i <= mid)
+  {
+    tmp[k++] = nums[i++];
+  }
+  while (j <= right)
+  {
+    tmp[k++] = nums[j++];
+  }
+  for (k = 0; k < len; k++)
+  {
+    nums[left + k] = tmp[k];
+  }
+}
+void merge_sort(int nums[], int left, int right)
+{
+  if (left >= right)
+    return;
+  int mid = (left + right) / 2;
+  merge_sort(nums, left, mid);
+  merge_sort(nums, mid + 1, right);
+  merge(nums, left, mid, right);
+}
+int main()
+{
+  int nums[] = {3, 1, 4, 2, 5}, len = sizeof nums / sizeof nums[0];
+  merge_sort(nums, 0, len - 1);
+  for (int i = 0; i < len; i++)
+  {
+    printf("%d ", nums[i]);
+  }
+  printf("\n");
+  return 0;
+}
